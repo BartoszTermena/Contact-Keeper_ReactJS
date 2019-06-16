84c808262e07a8bb4ab3d03f511ee29ab2df91c8
unknown <bartosz.termena@gmail.com>
2019-06-16 15:14:54 +0200

    Created notifications component, right bottom button is showing notifications in toast

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,16 +1,30 @@
 import React, { useEffect } from "react";
+import moment from "moment";
 
 const Notifications = props => {
   const { notifications } = props;
-  console.log(notifications);
-  return (
-    <div>
-      {notifications &&
-        notifications.map(item => {
-          return <div>{item.user}</div>;
-        })}
-    </div>
-  );
+  if (notifications && notifications.length > 0) {
+    return (
+      <div>
+        <ul className="online-users">
+          {notifications.map(item => {
+            return (
+              <li key={item.id}>
+                <span className="pink-text">{item.user} </span>
+                <span>{item.content}</span>
+                <div className="note-date grey-text">
+                  {moment(item.time.toDate()).fromNow()}
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+  if (notifications.length === 0) {
+    return <div>No notifications...</div>;
+  }
 };
 
 export default Notifications;
--- src/components/dashboard/Toast.js
+++ src/components/dashboard/Toast.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Notifications from "./Notifications";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const Toast = ({ notifications }) => {
-  console.log(notifications);
+  const notify = () => toast(<Notifications notifications={notifications} />);
+
   return (
     <div>
       <div>
-        <button>Notify !</button>
+        <div className="fixed-action-btn">
+          <a onClick={notify} className="btn-floating btn-large blue darken-2">
+            <i className="large material-icons">notifications</i>
+          </a>
+        </div>
         <ToastContainer />
       </div>
     </div>
