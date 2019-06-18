5e4e8d511c7c9dcd13b818524f2b8f29f685c1c4
unknown <bartosz.termena@gmail.com>
2019-06-18 16:46:33 +0200

    Started working on edit contact

--- src/components/dashboard/ContactSummary.js
+++ src/components/dashboard/ContactSummary.js
@@ -1,10 +1,16 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import moment from "moment";
 import { connect } from "react-redux";
 import { deleteContact } from "../../store/actions/contactActions";
 
 const ContactSummary = ({ contact, deleteContact }) => {
   const { id, name, email, phone, type, createdAt } = contact;
+
+  const [edit, setEdit] = useState(false);
+  const handleEdit = () => {
+    setEdit(!edit);
+  };
+
   return (
     <Fragment>
       <div className="card blue-grey darken-1">
@@ -13,10 +19,11 @@ const ContactSummary = ({ contact, deleteContact }) => {
             {name}
             <span className="card-status">{type}</span>
           </span>
-          <p>
-            <i className="tiny material-icons">email</i> {email}
+          <p className="contact-label">
+            <i className="tiny material-icons">email</i>
+            {!edit ? email : <input />}
           </p>
-          <p>
+          <p className="contact-label">
             <i className="tiny material-icons">local_phone</i> {phone}
           </p>
           <p className="grey-text">
@@ -24,7 +31,9 @@ const ContactSummary = ({ contact, deleteContact }) => {
           </p>
         </div>
         <div className="card-action">
-          <a className="btn-action">Edit</a>
+          <a className="btn-action" onClick={() => handleEdit()}>
+            Edit
+          </a>
           <a className="btn-action" onClick={() => deleteContact(id)}>
             Delete
           </a>
